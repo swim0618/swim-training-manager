2389594d6c87cd10a1f5f5be46691de554ec1d53
Fukudomekyosuke <swim.0815@icloud.com>
2025-12-29 15:52:18 +0900

    Spring Initializeでspring Boot project

---
+++ src/main/java/com/example/swim_training_manager/SwimTrainingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.swim_training_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimTrainingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimTrainingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swim_training_manager/SwimTrainingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swim_training_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimTrainingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
